7f233e77b45b14a1f3985b79291aa70402ac3799
Fatemeh Atighi <Fatemehatighi@gmail.com>
2026-02-15 01:13:18 +0330

    Handle Confirmation Message for create and edit note

---
+++ frontend/src/components/ConfirmationModal.tsx
@@ -0,0 +1,40 @@
+
+interface ConfirmationModalProps {
+  isOpen: boolean;
+  message: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+const ConfirmationModal = ({
+  isOpen,
+  message,
+  onConfirm,
+  onCancel,
+}: ConfirmationModalProps) => {
+  if (!isOpen) return null; 
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+      <div className="bg-blue-950 rounded-lg p-6 w-120 text-center">
+        <p className="mb-4">{message}</p>
+        <div className="flex justify-around gap-4">
+          <button
+            onClick={onConfirm}
+            className="bg-blue-800 text-white px-4 py-2 rounded hover:bg-blue-400"
+          >
+            بله
+          </button>
+          <button
+            onClick={onCancel}
+            className="bg-blue-800 px-4 py-2 rounded hover:bg-gray-400"
+          >
+            خیر
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationModal;
--- frontend/src/pages/NotesPage.tsx
+++ frontend/src/pages/NotesPage.tsx
@@ -1,83 +1,4 @@
-// import { useState } from "react";
-// import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-// import { getNotes, createNote, updateNote } from "../api/notes";
-// import type {Note} from '../types/note';
-// import { toast } from "react-hot-toast";
-// import NoteList from "../components/NotesList";
-// import NoteModal from "../components/NoteModal";
-// const NotesPage = () => {
-//   const queryClient = useQueryClient();
-//   const [mode, setMode] = useState<"create" | "edit" | null>(null);
-//   const [selectedNote, setSelectedNote] = useState<Note | null>(null);
-//   const { data: notes = [], isLoading } = useQuery({
-//     queryKey: ["notes"],
-//     queryFn: getNotes,
-//   });
-
-
-//   const createMutation = useMutation({
-//     mutationFn: createNote,
-//     onSuccess: () => {
-//       toast.success("Note created");
-//       queryClient.invalidateQueries({ queryKey: ["notes"] });
-//       setMode(null);
-//     },
-//   });
-// const updateMutation = useMutation<
-//   unknown,
-//   Error,
-//   { id: string; title: string; content: string }
-// >({
-//   mutationFn: updateNote,
-//   onSuccess: () => {
-//     toast.success("Note updated");
-//     queryClient.invalidateQueries({ queryKey: ["notes"] });
-//     setMode(null);
-//     setSelectedNote(null);
-//   },
-// });
-//   if (isLoading) return <p>Loading...</p>;
-//   return (
-//     <div>
-//       <div className="flex justify-between mb-6">
-//         <h2 className="text-2xl font-semibold">All Notes</h2>
-//       </div>
-//       <NoteList
-//         notes={notes}
-//         onEdit={(note) => {
-//           setSelectedNote(note);
-//           setMode("edit");
-//         }}
-//       />
-//       {mode && (
-//         <NoteModal
-//         setMode("create");
-//         setSelectedNote(null); 
-
-//           mode={mode}
-//           note={selectedNote}
-//           isLoading={
-//             mode === "edit"
-//               ? updateMutation.isPending
-//               : createMutation.isPending
-//           }
-//           onClose={() => setMode(null)}
-//           onSave={(data) => {
-//             if (mode === "edit" && selectedNote) {
-//               updateMutation.mutate({
-//                 id: selectedNote._id,
-//                 ...data,
-//               });
-//             } else {
-//               createMutation.mutate(data);
-//             }
-//           }}
-//         />
-//       )}
-//     </div>
-//   );
-// };
-// export default NotesPage;
+import ConfirmationModal from "../components/ConfirmationModal";
 
 import { useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
@@ -88,7 +9,18 @@ import NoteList from "../components/NotesList";
 import NoteModal from "../components/NoteModal";
 import { Plus } from "lucide-react";
 
+
+
+
+// const handleSave = (data: { title: string; content: string }) => {
+//   setPendingData(data);
+//   setConfirmOpen(true); // باز کردن confirmation
+// };
+
 const NotesPage = () => {
+const [confirmOpen, setConfirmOpen] = useState(false);
+const [pendingData, setPendingData] = useState<{ title: string; content: string } | null>(null);
+
   const queryClient = useQueryClient();
   const [mode, setMode] = useState<"create" | "edit" | null>(null);
   const [selectedNote, setSelectedNote] = useState<Note | null>(null);
@@ -155,24 +87,47 @@ const NotesPage = () => {
 
 
       {mode && (
-        <NoteModal
-          mode={mode}
-          note={selectedNote}
-          isLoading={
-            mode === "edit" ? updateMutation.isPending : createMutation.isPending
-          }
-          onClose={() => {
-            setMode(null);
-            setSelectedNote(null);
-          }}
-          onSave={(data) => {
-            if (mode === "edit" && selectedNote) {
-              updateMutation.mutate({ id: selectedNote._id, ...data });
-            } else {
-              createMutation.mutate(data);
-            }
-          }}
-        />
+        <><NoteModal
+  mode={mode}
+  note={selectedNote}
+  isLoading={
+    mode === "edit"
+      ? updateMutation.isPending
+      : createMutation.isPending
+  }
+  onClose={() => {
+    setMode(null);
+    setSelectedNote(null);
+  }}
+  onSave={(data) => {
+
+    setPendingData(data);
+    setConfirmOpen(true);
+  }}
+/>
+
+          <ConfirmationModal
+            isOpen={confirmOpen}
+            message={mode === "edit"
+              ? "Are you sure wanna edit this note..?"
+              : "Are you sure wanna create new note..?"}
+            onConfirm={() => {
+              if (!pendingData) return;
+              if (mode === "edit" && selectedNote) {
+                updateMutation.mutate({ id: selectedNote._id, ...pendingData });
+              } else {
+                createMutation.mutate(pendingData);
+              }
+              setConfirmOpen(false);
+              setPendingData(null);
+              setMode(null);
+              setSelectedNote(null);
+            } }
+            onCancel={() => {
+              setConfirmOpen(false);
+              setPendingData(null);
+            } } />
+            </>
       )}
     </div>
   );
