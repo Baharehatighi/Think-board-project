8ba7a908a690360e1e2369135b460ef4b86f9c10
Fatemeh Atighi <Fatemehatighi@gmail.com>
2026-02-15 14:55:45 +0330

    searchbutton

--- frontend/src/components/NoteCard.tsx
+++ frontend/src/components/NoteCard.tsx
@@ -1,23 +1,4 @@
 
-// import type { Note } from "../types/note";
-// import EditBtn from "./EditBtn";
-// type Props = {
-//   note: Note;
-// };
-
-
-// const NoteCard = ({ note }: Props) => {
-//   return (
-//     <div className="bg-gray-700 p-4 rounded-xl shadow">
-//       <h3 className="font-bold text-white">Title: {note.title}</h3>
-//       <p className="text-sm text-white"> Content: {note.content}</p>
-//       <EditBtn/>
-//     </div>
-    
-//   );
-// };
-
-// export default NoteCard;
 import { Edit } from "lucide-react";
 import type { Note } from "../types/note";
 
--- frontend/src/components/NotesList.tsx
+++ frontend/src/components/NotesList.tsx
@@ -1,23 +1,4 @@
 
-// import type { Note } from "../types/note";
-// import NoteCard from "./NoteCard";
-
-// type Props = {
-//   notes: Note[];
-// };
-
-// const NotesList = ({ notes }: Props) => {
-//   return (
-    
-//     <div className="grid grid-cols-3 gap-4">
-//       {notes.map((note) => (
-//         <NoteCard key={note._id} note={note} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default NotesList;
 import type { Note } from "../types/note";
 import NoteCard from "./NoteCard";
 
